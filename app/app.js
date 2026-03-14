e1cce090929c815d9b08b1a018fdc47b815263c6
vohrr <matthew.d.pingel@gmail.com>
2026-03-13 21:11:56 -0400

    cache bust

--- app/app.js
+++ app/app.js
@@ -261,7 +261,8 @@ function viewVideo(video) {
     thumbnailImg.style.display = 'none';
   } else {
     thumbnailImg.style.display = 'block';
-    thumbnailImg.src = video.thumbnail_url;
+    originalUrl = video.thumnail_url;
+    thumbnailImg.src = `${originalUrl}?v=${Date.now()}`;
   }
 
   const videoPlayer = document.getElementById('video-player');
